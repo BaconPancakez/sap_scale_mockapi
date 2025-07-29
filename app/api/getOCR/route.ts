4d3c7860145e2b35d1a6c404125c70ced7b8f4ec
Qi Ming <qmseng15@gmail.com>
2025-07-29 12:29:54 +0800

    update failure rate

--- app/api/getOCR/route.ts
+++ app/api/getOCR/route.ts
@@ -7,9 +7,9 @@ export async function GET() {
   let failedItem: string | null = null;
   const randomPercent = Math.random() * 100;
 
-  if (randomPercent < 30) { // 10% chance of major failure
+  if (randomPercent < 40) { // 10% chance of major failure
     failedItem = majorIssues[Math.floor(Math.random() * majorIssues.length)];
-  } else if (randomPercent < 60) { // 20% chance of minor issue
+  } else if (randomPercent < 80) { // 20% chance of minor issue
     failedItem = minorIssues[Math.floor(Math.random() * minorIssues.length)];
   }
 
